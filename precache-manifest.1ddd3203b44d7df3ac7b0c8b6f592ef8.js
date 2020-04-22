aed162ce5b9f9601b488e5b6a0bb050dce1bf38a
greg archer <nihlton@gmail.com>
2020-04-21 18:01:37 -0700

    add demo

---
+++ precache-manifest.1ddd3203b44d7df3ac7b0c8b6f592ef8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc60e2cb68b91602ff9c7ef3e0f253b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd4686d63f6c0b7335a7",
+    "url": "/static/css/2.24487d18.chunk.css"
+  },
+  {
+    "revision": "d922196c7a6c0652eb68",
+    "url": "/static/css/main.9e8e304b.chunk.css"
+  },
+  {
+    "revision": "cd4686d63f6c0b7335a7",
+    "url": "/static/js/2.e5c34306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5c34306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d922196c7a6c0652eb68",
+    "url": "/static/js/main.40521a09.chunk.js"
+  },
+  {
+    "revision": "d52caff26e5729bd50d1",
+    "url": "/static/js/runtime-main.7bbb480a.js"
+  }
+]);
