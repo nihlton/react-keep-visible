0edad3ab396aec1b877074a5192300df567b8550
greg archer <nihlton@gmail.com>
2020-10-17 21:54:52 -0700

    rework logic to leverage position sticky

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 ![React Keep Visible](https://repository-images.githubusercontent.com/257725090/ea52cb00-8486-11ea-80f1-d93c94bf0bea)
 
-# React Keep Visisble [BETA]
+# React Keep Visible [BETA]
 Keep an element in the viewport (within the bounds of its parent container) while the user scrolls.
 
-Live demo: https://nihlton.github.io/react-keep-visible/
+Similar to position 'sticky', except it intelligently switches between sticking to the top and bottom 
+edge to allow scrolling through the entire element.
+
+[live demo](https://codesandbox.io/s/keep-visible-demo-1x1fw?file=/src/App.js)
+
+Vanilla sticky: [counter demo](https://codesandbox.io/s/keep-visible-counter-demo-sumq9?file=/src/App.js) 
+
 
 ## Installation
 
@@ -19,9 +25,10 @@ or
       <KeepVisible>
         [ element ]
       </KeepVisible>
-
 ```
 
 ## Notes
+
+To do:  support 'top' & 'bottom' offsets.
 
 Margins added to the element kept visible will negatively impact positioning.  Try padding instead.
--- src/index.js
+++ src/index.js
@@ -1,80 +1,100 @@
-import React, { useEffect, useLayoutEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
+
+const STICKY_BOTTOM = 2
+const STICKY_TOP = 1
+
+const PARENT_STYLE = {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+}
 
 const KeepVisible = function (props) {
+  const parentRef = useRef()
   const childRef = useRef()
-  let alreadyHandling
-  let lastScrollPosition
+  const childSticky = useRef(-1)
+  const scrollY = useRef(window.scrollY)
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScrollAction)
-    window.addEventListener('resize', handleScrollAction)
+    const positionChild = () => {
+      const child = childRef.current
+      const parent = parentRef.current
+
+      if (!child || !parent) { return }
+
+      const parentRect = parent.getBoundingClientRect()
+      const childRect = child.getBoundingClientRect()
+
+      const scrollingUp = scrollY.current > window.scrollY
+      const scrollingDown = scrollY.current < window.scrollY
+      const changeDown = scrollingDown && childSticky.current === STICKY_TOP
+      const changeUp = scrollingUp && childSticky.current === STICKY_BOTTOM
+      const changeDirection = changeDown || changeUp
+
+      const viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+      const fitsInViewPort = childRect.height < viewPortHeight
+
+      const childAboveViewBottom = (childRect.top) < (viewPortHeight - childRect.height)
+      const childBelowViewTop = (childRect.top) > 0
+      const childInMiddle = !childBelowViewTop && !childAboveViewBottom
+      const childEdgeInView = childBelowViewTop || childAboveViewBottom
+
+      if (fitsInViewPort) {
+        parent.style.justifyContent = ''
+        child.style.position = 'sticky'
+        child.style.top = 0
+        child.style.bottom = ''
+        return
+      }
+
+      const childStyle = {}
+      const parentStyle = { ...PARENT_STYLE }
+
+      if (changeDirection && childInMiddle) {
+        childSticky.current = false
+        parentStyle.justifyContent = ''
+        childStyle.position = 'relative'
+        childStyle.top = (childRect.top - parentRect.top) + 'px'
+        childStyle.bottom = ''
+      }
+
+      if (scrollingDown && childEdgeInView) {
+        childSticky.current = STICKY_BOTTOM
+        parentStyle.justifyContent = 'flex-end'
+        childStyle.position = 'sticky'
+        childStyle.bottom = 0
+        childStyle.top = ''
+      }
+
+      if (scrollingUp && childEdgeInView) {
+        childSticky.current = STICKY_TOP
+        parentStyle.justifyContent = ''
+        childStyle.position = 'sticky'
+        childStyle.top = 0
+        childStyle.bottom = ''
+      }
+
+      Object.assign(parent.style, parentStyle)
+      Object.assign(child.style, childStyle)
+
+      scrollY.current = window.scrollY
+    }
+
+    window.addEventListener('scroll', positionChild, { passive: true })
+    window.addEventListener('resize', positionChild, { passive: true })
     positionChild()
 
     return () => {
-      window.removeEventListener('scroll', handleScrollAction)
-      window.removeEventListener('resize', handleScrollAction)
+      window.removeEventListener('scroll', positionChild, { passive: true })
+      window.removeEventListener('resize', positionChild, { passive: true })
     }
-  }, [ childRef ])
+  }, [childRef])
 
-  useLayoutEffect(() => positionChild(), [ childRef ])
-  
-  const handleScrollAction = () => {
-    lastScrollPosition = window.scrollY
-    positionChild()
-  }
-
-  const positionChild = () => {
-    if (alreadyHandling) { return }
-    alreadyHandling = true
-
-    window.requestAnimationFrame(() => {
-      alreadyHandling = false
-      
-      const child = childRef.current
-      const parent = child.parentElement
-
-      if (!child || !parent) { return }
-  
-      const notScrolling = lastScrollPosition === window.scrollY
-      const scrollingUp = lastScrollPosition > window.scrollY
-      const scrollingDown = lastScrollPosition < window.scrollY
-  
-      const childRect = child.getBoundingClientRect()
-      const parentRect = parent.getBoundingClientRect()
-      
-      const viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-      const fitsInViewPort = childRect.height < viewPortHeight
-  
-      const childAboveViewBottom = (childRect.top) < (viewPortHeight - childRect.height)
-      const childBelowViewTop = (childRect.top) > 0
-      
-      const minPosition = 0
-      const maxPosition = Math.max(parentRect.height - childRect.height, 0)
-  
-      let newTop = false
-  
-      if (fitsInViewPort) { newTop = -parentRect.top }
-      if (!fitsInViewPort) {
-        if ((notScrolling || scrollingUp) && childBelowViewTop) {
-          newTop = -parentRect.top
-        }
-        if ((notScrolling || scrollingDown) && childAboveViewBottom) {
-          newTop = (-parentRect.top + viewPortHeight) - childRect.height - 1
-        }
-      }
-      
-      if (newTop !== false) {
-        newTop = Math.max(newTop, minPosition)
-        newTop = Math.min(newTop, maxPosition)
-        child.style.transform = `translateY( ${newTop}px)`
-      }
-    })
-  }
-  
-  return <div ref={childRef}>
-    {props.children}
-  </div>
-  
+  return (
+    <div ref={parentRef} style={{ height: '100%' }}>
+      <div ref={childRef}>{props.children}</div>
+    </div>
+  )
 }
 
 export default KeepVisible
